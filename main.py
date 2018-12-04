491b9ada54d8dc752102ec6a87aceb9b3e70204b
Jared H <jaredhwang@gmail.com>
2018-12-04 13:53:19 -0500

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from gameInput import gameInput
 from canvas import Canvas
 from PyQt5.Qt import QColor
@@ -6,13 +7,16 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPainter, QBrush, QPen
 from PyQt5.QtCore import Qt
 
-# game = gameInput()
 
-# game.promptUser()
-# test = game.acceptInput()
+# app = QApplication(sys.argv)
+# canvas = Canvas()
+# canvas.show()
+
+game = gameInput()
+
+game.promptUser()
+test = game.acceptInput()
+
+# sys.exit(app.exec_())
 
 
-app = QApplication(sys.argv)
-canvas = Canvas()
-canvas.show()
-sys.exit(app.exec_())
